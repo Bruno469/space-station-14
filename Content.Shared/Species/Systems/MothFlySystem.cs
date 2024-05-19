a713a1451f33601aea5a016789891ede39784c63
bruno469 <bruno319124@gmail.com>
2024-05-19 16:55:52 -0300

    Add basic fly

--- Content.Shared/Species/Systems/MothFlySystem.cs
+++ Content.Shared/Species/Systems/MothFlySystem.cs
@@ -6,6 +6,11 @@ using Content.Shared.Mobs.Components;
 using Content.Shared.Popups;
 using Robust.Shared.Prototypes;
 
+using Content.Shared.Damage.Systems;
+using Content.Shared.Movement.Events;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Systems;
+using Content.Shared.Movement.Systems;
 
 namespace Content.Shared.Species;
 
@@ -15,17 +20,20 @@ public sealed partial class MothFlySystem : EntitySystem
     [Dependency] private readonly SharedBodySystem _bodySystem = default!;
     [Dependency] private readonly IPrototypeManager _protoManager = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifier = default!;
+    [Dependency] private readonly StaminaSystem _stamina = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<MothFlyComponent, MobStateChangedEvent>(OnMobStateChanged);
-        SubscribeLocalEvent<MothFlyComponent, BaseActionEvent>(OnFlyAction);
+        SubscribeLocalEvent<MothFlyComponent, ToggleMothflyEvent>(OnFlyAction);
     }
 
     private void OnMobStateChanged(EntityUid uid, MothFlyComponent comp, MobStateChangedEvent args)
     {
-        // When the mob changes state, check if they're dead and give them the action if so. 
+        // When the mob changes state, check if they're dead and give them the action if so.
         if (!TryComp<MobStateComponent>(uid, out var mobState))
             return;
 
@@ -46,8 +54,12 @@ public sealed partial class MothFlySystem : EntitySystem
 
     private void OnFlyAction(EntityUid uid, MothFlyComponent comp, BaseActionEvent args)
     {
-        // When they use the action, gib them.
+        // When they use the action, gib them (It's a meme lol).
         _popupSystem.PopupClient(Loc.GetString(comp.PopupText, ("name", uid)), uid, uid);
-        _bodySystem.GibBody(uid, true);
+        if (TryComp<PhysicsComponent>(uid, out var physics))
+            _physics.SetBodyStatus(uid, physics, BodyStatus.InAir);
+            _movementSpeedModifier.ChangeBaseSpeed(uid, 2, 2, 1);
+            _bodySystem.UpdateMovementSpeed(uid);
+            _stamina.TakeStaminaDamage(uid, 50);
     }
 }
