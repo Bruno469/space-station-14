404d47c4f6dc7d214532cfc16787089873db76de
bruno469 <bruno319124@gmail.com>
2024-05-29 13:32:31 -0300

    all erro fixed

--- Content.Server/Movement/Systems/MothFlySystem.cs
+++ Content.Server/Movement/Systems/MothFlySystem.cs
@@ -26,23 +26,15 @@ public sealed class MothFlySystem : SharedMothFlySystem
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly StaminaSystem _stamina = default!;
     [Dependency] private readonly AtmosphereSystem _atmosphere = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
 
     protected override bool CanEnable(EntityUid uid, MothFlyComponent component)
     {
         var uidXform = Transform(uid);
         var coordinates = uidXform.Coordinates;
         var gridUid = coordinates.GetGridUid(EntityManager);
-
-        if (TryComp<MapGridComponent>(gridUid, out var grid))
-        {
-            coordinates = _transform.GetGridTilePositionOrDefault((gridUid, grid));
-        }
-        else
-        {
-            return false;
-        }
-
-        var tile = _atmosphere.GetTileMixture(gridUid, null, coordinates, true);
+        var pos = coordinates.ToVector2i(EntityManager, _mapManager, _transform);
+        var tile = _atmosphere.GetTileMixture(gridUid, null, pos, true);
         if (tile != null)
             return true;
         return base.CanEnable(uid, component);
@@ -55,14 +47,14 @@ public sealed class MothFlySystem : SharedMothFlySystem
         var toDisable = new ValueList<(EntityUid Uid, MothFlyComponent Component)>();
         var query = EntityQueryEnumerator<ActiveJetpackComponent, MothFlyComponent>();
 
-        while (query.MoveNext(out var uid, out var active, out var comp))
+        while (query.MoveNext(out var uid, out var active))
         {
             if (_timing.CurTime < active.TargetTime)
                 continue;
 
             active.TargetTime = _timing.CurTime + TimeSpan.FromSeconds(active.EffectCooldown);
 
-            if (TryComp<PhysicsComponent>(uid, out var physics))
+            if (TryComp<PhysicsComponent>(uid))
                 _stamina.TakeStaminaDamage(uid, 15);
         }
 
